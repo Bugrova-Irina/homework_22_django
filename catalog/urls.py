dba124b004a69ae6e0fb02cf92cc5436e3ef17fd
Bugrova Irina <bugrova147@yandex.ru>
2025-04-29 13:52:06 +0300

    add templates home.html, contacts.html

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for catalog project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -15,13 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path
-from . import views
+from catalog.views import home, contacts
+from catalog.apps import CatalogConfig
 
-app_name = 'catalog'
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.catalog, name='catalog')
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
 ]
