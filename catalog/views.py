eda180b9aadf95cb7045938378b71d4630eb71c7
Bugrova Irina <bugrova147@yandex.ru>
2025-05-13 11:25:06 +0300

    add product_detail.html, base.html, inc_footer.html, inc_header.html, inc_menu.html, my_tags.py, fixed cards of products, add new products, fixed views.py, catalog/urls.py, home.html

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-def home(request):
-    return render(request, 'catalog/home.html')
+from catalog.models import Product
 
 
 def contacts(request):
@@ -13,3 +12,15 @@ def contacts(request):
 
         return HttpResponse(f"Спасибо, {name} ({phone})! Сообщение получено: {message}.")
     return render(request, 'catalog/contacts.html')
+
+
+def home(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'catalog/home.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'catalog/product_detail.html', context)
