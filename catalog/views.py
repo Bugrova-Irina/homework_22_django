89678fc41785eb7b2702c4812f60726887e65cf8
Bugrova Irina <bugrova147@yandex.ru>
2025-05-19 15:08:02 +0300

    views.py: add classes ProductsListView, ProductDetailView, ContactsView; fixed contacts.html, urls.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,26 +1,42 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, TemplateView
 
 from catalog.models import Product
 
 
-def contacts(request):
-    if request.method == "POST":
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+    context_object_name = 'products'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
+
+
+class ContactsView(TemplateView):
+    template_name = 'catalog/contacts.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
         name = request.POST.get("name")
         phone = request.POST.get("phone")
         message = request.POST.get("message")
-
         return HttpResponse(f"Спасибо, {name} ({phone})! Сообщение получено: {message}.")
-    return render(request, 'catalog/contacts.html')
 
 
-def home(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, 'catalog/home.html', context)
+# def home(request):
+#     products = Product.objects.all()
+#     context = {"products": products}
+#     return render(request, 'catalog/home.html', context)
 
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, 'catalog/product_detail.html', context)
+# def product_detail(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     context = {"product": product}
+#     return render(request, 'catalog/product_detail.html', context)
